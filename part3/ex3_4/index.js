18c0e880c6b1e74ada31f31106f98a872a3b0b0e
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-02 18:32:39 +0300

    exercise4

---
+++ part3/ex3_4/index.js
@@ -0,0 +1,70 @@
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    { 
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    { 
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.end(JSON.stringify(persons))
+  })
+// http://localhost:3001/api/persons
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+// http://localhost:3001/api/persons/1
+
+app.get('/info', (request, response) => {
+    const num = persons.length
+    const now = new Date().toString();
+    const resp = "Phonebook has info for " + num + " people <br/> " + now
+
+    response.send(resp)
+  })
+// http://localhost:3001/info
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+}) // tested with postman and it worked
+
+  
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
